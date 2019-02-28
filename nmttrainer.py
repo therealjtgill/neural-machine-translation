f1e40af1cc98bf27fade6b365a7cf9a9c6298854
therealjtgill <jtgill@email.arizona.edu>
2019-02-27 23:03:53 -0700

    Added preliminary training with english-french data. Fixed timestep training.

--- nmttrainer.py
+++ nmttrainer.py
@@ -1,30 +1,53 @@
-import argarse
+import argparse
 import json
 import matplotlib.pyplot as plt
 from nmt import NMT
 import numpy as np
 import os
+import sys
+import tensorflow as tf
+from tokenizedloader import DataHandler
 
 def main(argv):
   parser = argparse.ArgumentParser(description="Script to train a Neural Machine Translation Model.\
     This is based off of the work by Bahdanau; an encoder/decoder model with attention.")
 
-  parser.add_argument("-e", "--englishtext",
+  parser.add_argument("-et", "--englishtext",
     required    = True,
     help        = "The location of tokenized english training text.")
 
-  parser.add_argument("-t", "--targettext",
+  parser.add_argument("-tt", "--targettext",
     required    = True,
     help        = "The location of tokenized training text in the target language.")
 
+  parser.add_argument("-ed", "--englishdict",
+    required    = True,
+    help        = "The location of the dictionary that translates english words to tokens.")
+
+  parser.add_argument("-td", "--targetdict",
+    required    = True,
+    help        = "The location of the dictionary that translates target language words to tokens.")
+
   parser.add_argument("-b", "--batchsize",
     required    = False,
-    default     = 64,
+    default     = 20,
     help        = "The size of the training batches to use.")
 
   args = parser.parse_args()
 
-  
+  dh = DataHandler(args.englishtext, args.englishdict, args.targettext, args.targetdict)
+  sess = tf.Session()
+  nmt = NMT(sess, in_vocab_size=dh.vocab_sizes[0], out_vocab_size=dh.vocab_sizes[1])
+  sess.run(tf.global_variables_initializer())
+
+  losses = []
+
+  for i in range(1000):
+    new_batch = dh.getTrainBatch(args.batchsize)
+    loss, _ = nmt.trainStep(new_batch[0], new_batch[1])
+    losses.append(loss)
+    print("train on, mothafucka")
+  print(losses)
 
 if __name__ == "__main__":
   main(sys.argv)
